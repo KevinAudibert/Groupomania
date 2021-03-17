3c464d0a7f51c6ad3c3da0e695963aa29366ab84
KevinAudibert <63927017+KevinAudibert@users.noreply.github.com>
2021-03-17 18:57:17 +0100

    rajout route list message

--- backend/routes/message.js
+++ backend/routes/message.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const messageCtrl = require('../controllers/message');
 
 router.post('/new', messageCtrl.createMessage)
+router.get('/', messageCtrl.listMessage)
 
 module.exports = router;
