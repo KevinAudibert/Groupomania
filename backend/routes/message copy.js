750f5d3d367b1432202f84ff87c50023b803a025
KevinAudibert <63927017+KevinAudibert@users.noreply.github.com>
2021-03-29 16:38:53 +0200

    delete file

--- backend/routes/message copy.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const likesCtrl = require('../controllers/like');
-
-router.post('/messages/:messageId/vote/like', likesCtrl.likePost )
-
-module.exports = router;
